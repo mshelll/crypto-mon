580bdf5f35dbed296674dd8835c4854adbcc224d
mba4cob <marshel.abraham@gmail.com>
2020-04-05 17:19:13 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # currency
+This code base requires golang version 1.14 or above because modules are resolved based on go modules
+
+To run the server make sure you have internet connection to download deps from github
+
+        go run currency.go
+
+
+To test the server
+
+        curl -X GET "http://localhost:8080/currency/all"
+        curl -X GET "http://localhost:8080/currency/ETHBTC"
+        curl -X GET "http://localhost:8080/currency/BTCUSD"
+
+CONGIURE allowed symbols
+
+    The file currency.config in the same dir can be used to configure which all symbols can be allowed
+    Even not configured ETHBSD and BTCUSD is allowed. During the start of server. We will fetch all 
+    valid public symbols by HTTP request and validate all configured symbols in currency.config against 
+    public symbols
+
+
+
+How I Resolved Dependecies
+
+From the currency dir run 
+       
+        go mod init currency
+
+Above command will download and install all dependecy packages if not already present
+For this execise I have used single external package "github.com/gorilla/websocket"
+Socket market data was transmitted over websocket. To listen to socket market data we need above package
+Alternative method is to use http request with a configurable polling time interval 
+
+
+
+
+
+
---
+++ currency.go
@@ -0,0 +1,168 @@
+package main 
+
+import (
+	"net/http"
+	"log"
+	"strings"
+	"github.com/gorilla/websocket"
+	"encoding/json"
+	"sync"
+	"os"
+	"bufio"
+)
+
+type Cache struct {
+
+	data map[string][]byte
+	mux sync.RWMutex
+}
+
+func (c *Cache) update(key string, val []byte) {
+
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.data[key] = val
+}
+
+func (c *Cache) fetch(key string) string {
+
+	c.mux.RLock()
+	defer c.mux.RUnlock()
+	val, _ := c.data[key]
+	return string(val)
+}
+
+var cache Cache
+var valid_symbols map[string]struct{}
+
+func currencyMonitor(symbol string) {
+
+	url := "wss://api.hitbtc.com/api/2/ws"
+	c, _, err := websocket.DefaultDialer.Dial(url, nil)
+	defer c.Close()
+	if err != nil {
+		log.Fatal("webscoket dial:", err)
+		return
+	}
+
+	message_tmpl := `{"method": "subscribeTicker", "params": {"symbol": "{symbol}" }, "id": 123}`
+	request_msg := strings.NewReplacer("{symbol}", symbol).Replace(message_tmpl)
+
+	if c.WriteMessage(websocket.TextMessage, []byte(request_msg)) != nil {
+
+		log.Fatal("websocket write:", err)
+		return
+	}
+
+	for {
+
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Fatal("webscoket read:", err)
+			return
+		}
+
+		var response map[string]interface{}
+		err = json.Unmarshal(message, &response)
+		if response["params"] != nil {
+			params := response["params"]
+			msg, _ := json.Marshal(params)
+			cache.update(symbol, msg)
+		}
+	}
+}
+
+func currencyHandler(w http.ResponseWriter, req *http.Request) {
+
+	url := req.URL.Path
+	symbol := strings.TrimLeft(url, "/currency/")
+
+
+	if symbol == "all" {
+
+		response := make(map[string][]string)
+		for symbol, _ := range(valid_symbols) {
+			responseString := cache.fetch(symbol)
+			response["currencies"] = append(response["currencies"], responseString)
+		}
+		json.NewEncoder(w).Encode(response)
+
+	} else if _, found := valid_symbols[symbol]; found {
+
+		response := cache.fetch(symbol)
+		json.NewEncoder(w).Encode(response)
+	} else {
+
+		response := `{"code": 400, "message": "Invalid Symbol"`
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func validateSymbols() {
+
+	file, _ := os.Open("currency.config")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var config_symbols []string
+
+	for scanner.Scan() {
+		config_symbols = append(config_symbols, scanner.Text())
+	}
+
+	type Symbol struct {
+
+		Id string
+	}
+
+	var symbols []Symbol
+
+	req_url := "https://api.hitbtc.com/api/2/public/symbol"  
+	response, err := http.Get(req_url)
+	defer response.Body.Close()
+	if err != nil {
+		log.Fatal("public symbol fetch :", err)
+	}
+	json.NewDecoder(response.Body).Decode(&symbols)
+
+	pub_symbols := make(map[string]struct{})
+
+	for _, symbol := range(symbols) {
+
+		pub_symbols[symbol.Id] = struct{}{}
+	}
+
+	for _, symbol := range(config_symbols) {
+
+		if _, found := pub_symbols[symbol]; found {
+
+			valid_symbols[symbol] = struct{}{}
+		} else {
+			log.Println("configured symbol not valid :", symbol)
+		}
+	}
+}
+
+func init() {
+
+	valid_symbols = map[string]struct{}{
+		"ETHBTC": struct{}{},
+		"BTCUSD": struct{}{},
+	}
+
+	cache.data = make(map[string][]byte)
+}
+
+func main() {
+
+	validateSymbols()
+
+	for symbol, _ := range(valid_symbols) {
+
+		go currencyMonitor(symbol)
+	}
+
+	http.HandleFunc("/currency/", currencyHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+} 
